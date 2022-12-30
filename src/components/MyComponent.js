c3471f74fdefe60a239ab531d5c635b92e4a96f1
Vo Hieu Nghia <nvohieeunghia@gmail.com>
2022-12-31 00:02:36 +0700

    video 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,13 +8,23 @@ class MyComponent extends React.Component{
         age: 20
     };
 
+    handleClick(event){
+        console.log("Click me my buttom");
+        console.log("My name Nghia ", this.state.name);
+
+    }
+
+    handleOnMoverOver(event){
+        console.log(event);
+    }
 
     //JSX
     render(){
         return(
             <div>
                 My name is {this.state.name} and I`m from {this.state.address}
-
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMoverOver}>Hover click</button>
             </div>
         );
     }
